dc2275d9e737b95b990eadfc93ada24e435089f3
kenzocarneiro <kenzo.carneiro.36@gmail.com>
2023-10-15 02:07:38 +0200

    fix warning

--- client/src/pages/Logas.jsx
+++ client/src/pages/Logas.jsx
@@ -1,6 +1,5 @@
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
 
 import Header from "./composant/Header";
 import Footer from "./composant/Footer";
